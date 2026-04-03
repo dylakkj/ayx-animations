9677d8d434d6d787982e27a1f28a580fe7484df4
Dyla <dylarlk@outlook.com>
2026-04-03 09:13:07 -0300

    version

--- open/updater/_version.lua
+++ open/updater/_version.lua
@@ -1,6 +1,6 @@
 -- This file is updated automatically by the updater.
 local AyxAnimationsUpdater = {}
 
-AyxAnimationsUpdater.Version = "1.0.1"
+AyxAnimationsUpdater.Version = "1.0.2" -- DEVELOPMENT
 
 return AyxAnimationsUpdater
