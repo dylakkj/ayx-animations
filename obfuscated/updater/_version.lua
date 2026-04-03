a0fefe93c5f18a1d23e720c5488f4b1ecef2f90f
Dyla <dylarlk@outlook.com>
2026-04-03 09:29:49 -0300

    update vers

--- obfuscated/updater/_version.lua
+++ obfuscated/updater/_version.lua
@@ -1,6 +1,6 @@
 -- This file is updated automatically by the updater.
 local AyxAnimationsUpdater = {}
 
-AyxAnimationsUpdater.Version = "1.0.2" -- DEPLOY
+AyxAnimationsUpdater.Version = "1.0.3" -- DEPLOY
 
 return AyxAnimationsUpdater
--- open/updater/_version.lua
+++ open/updater/_version.lua
@@ -1,6 +1,6 @@
 -- This file is updated automatically by the updater.
 local AyxAnimationsUpdater = {}
 
-AyxAnimationsUpdater.Version = "1.0.2" -- DEVELOPMENT
+AyxAnimationsUpdater.Version = "1.0.3" -- DEVELOPMENT
 
 return AyxAnimationsUpdater
