0b52408d0aa3011cbc0cd18c7ddabc9a6828e64d
Dyla <dylarlk@outlook.com>
2026-04-03 09:50:49 -0300

    1.0.4

--- open/fxmanifest.lua
+++ open/fxmanifest.lua
@@ -1,36 +1,32 @@
-fx_version "bodacious"
-game "gta5"
-lua54 "yes"
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
-ui_page "html/index.html"
+ui_page 'html/index.html'
 
 files {
-	"html/**/*",
-	"custom.lua"
+	'html/**/*',
+	'custom.lua'
 }
 
 shared_script {
-	"adapter/config.shared.lua"
+	'adapter/config.shared.lua'
 }
 
 client_scripts {
-	"@vrp/lib/utils.lua",
-	"adapter/locale.client.lua",
-	"adapter/animations.client.lua",
-	"adapter/core.client.lua",
+	'@vrp/lib/utils.lua',
+	'adapter/locale.client.lua',
+	'adapter/animations.client.lua',
+	'adapter/core.client.lua',
 
-	"custom.lua",
+	'custom.lua',
 
 }
 
 server_scripts {
-	"@vrp/lib/utils.lua",
-	"updater/_version.lua",
-	"updater/_server.lua",
-	"adapter/core.server.lua",
+	'@vrp/lib/utils.lua',
+	'updater/_version.lua',
+	'updater/_server.lua',
+	'adapter/core.server.lua',
 
-}
-
-escrow_ignore {
-	"custom.lua",
 }
