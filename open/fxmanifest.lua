5a7a0666898d86606b5cc8fac68ad9256c7ee792
Dyla <dylarlk@outlook.com>
2026-04-03 09:46:19 -0300

    Revert "update manifest"
    This reverts commit 3da2f7153ce6c3d2203558a1b9bf0584df93485f.

--- open/fxmanifest.lua
+++ open/fxmanifest.lua
@@ -1,34 +1,34 @@
-fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
+fx_version "bodacious"
+game "gta5"
+lua54 "yes"
 
-ui_page 'html/index.html'
+ui_page "html/index.html"
 
 files {
-	'html/**/*',
-	'stream/*.ytyp',
-	'custom.lua'
+	"html/**/*",
+	"custom.lua"
 }
 
 shared_script {
-	'adapter/config.shared.lua'
+	"adapter/config.shared.lua"
 }
 
 client_scripts {
-	'@vrp/lib/utils.lua',
-	'adapter/locale.client.lua',
-	'adapter/animations.client.lua',
-	'adapter/core.client.lua',
-	'custom.lua',
+	"@vrp/lib/utils.lua",
+	"adapter/locale.client.lua",
+	"adapter/animations.client.lua",
+	"adapter/core.client.lua",
+
+	"custom.lua",
 
 }
 
 server_scripts {
-	'@vrp/lib/utils.lua',
-	'updater/_version.lua',
-	'updater/_server.lua',
-	'adapter/core.server.lua',
+	"@vrp/lib/utils.lua",
+	"updater/_version.lua",
+	"updater/_server.lua",
+	"adapter/core.server.lua",
 
 }
 
-data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
+data_file "DLC_ITYP_REQUEST" "stream/hype_folia_flags"
